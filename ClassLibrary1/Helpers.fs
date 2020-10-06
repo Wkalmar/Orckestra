3b017d6033e36e94d064bac46751a3010dd28889
Bohdan Stupak <stupakbogdan@ukr.net>
2020-10-06 11:03:59 +0300

    use val instead of static

--- ClassLibrary1/Helpers.fs
+++ ClassLibrary1/Helpers.fs
@@ -9,27 +9,30 @@ type PermissionInfo(symbol: char, value: int) =
     member x.Value = value
 
 
-[<IsByRefLike; Struct>]
+//[<IsByRefLike; Struct>]
 type Helpers =
-    static member private Permissions =
+    val private Permissions : PermissionInfo[]
+    new () = {
+        Permissions = 
         [|PermissionInfo('r', 4);
-          PermissionInfo('w', 2);
-          PermissionInfo('x', 1);
-          PermissionInfo('r', 4);
-          PermissionInfo('w', 2);
-          PermissionInfo('x', 1);
-          PermissionInfo('r', 4);
-          PermissionInfo('w', 2);
-          PermissionInfo('x', 1);
-          PermissionInfo('r', 4);
-          PermissionInfo('w', 2);
-          PermissionInfo('x', 1); |]
+        PermissionInfo('w', 2);
+        PermissionInfo('x', 1);
+        PermissionInfo('r', 4);
+        PermissionInfo('w', 2);
+        PermissionInfo('x', 1);
+        PermissionInfo('r', 4);
+        PermissionInfo('w', 2);
+        PermissionInfo('x', 1);
+        PermissionInfo('r', 4);
+        PermissionInfo('w', 2);
+        PermissionInfo('x', 1); |]
+    }    
 
     member x.ConvertBlockToOctal (block : ReadOnlySpan<char>) =
         let mutable acc = 0
-        for i = 0 to Helpers.Permissions.Length - 1 do
-            if block.[i] = Helpers.Permissions.[i].Symbol then
-                acc <- acc + Helpers.Permissions.[i].Value
+        for i = 0 to x.Permissions.Length - 1 do
+            if block.[i] = x.Permissions.[i].Symbol then
+                acc <- acc + x.Permissions.[i].Value
             else
                 acc <- acc
         acc
